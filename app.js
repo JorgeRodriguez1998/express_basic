b2a930d07813d98c3571cd529e37b42e0b6c476a
JorgeRodriguez1998 <jorgerodriguez5tos@gmail.com>
2022-05-02 12:32:13 -0500

    Adding a new route and returning a list of objects

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const port = 3000; //localhost:3000
 
 // localhost:3000
 app.get('/', (req, res) => {
-    res.send("Hola a todos mis explorers")
+    res.send("Hola a todos mis explorers");
 })
 
 //localhost:300/launchx
 app.get('/launchx', (req, res) => {
-    res.send("Bienvenidos a launchX")
+    res.send("Bienvenidos a launchX");
+})
+
+//localhost:300/node
+app.get('/node', (req, res) => {
+    const explorer1 = {id: 1, name: "Explorer"}
+    const explorer2 = {id: 2, name: "Explorer"}
+    const explorer3 = {id: 3, name: "Explorer"}
+    res.send([explorer1, explorer2,explorer3]);
 })
 
 
